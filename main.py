90faaf2d6492cc5130a8aa42e67bdc3f78e23e70
j-kirkeby <j-kirkeby@outlook.com>
2025-05-30 22:13:38 +0200

    CH2-2: schmoovin

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    # Group-ish
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    dt = 0
+
 
     while True:
         # Close button
@@ -14,8 +25,15 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        # Draw background
         screen.fill("black")
+
+        updatable.update(dt)
+        for drawie in drawable:
+            drawie.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
